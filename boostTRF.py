451dc40d9e96ebe451f7290c30e8a88f0653f494
Joshua Kulasingham <pranjeevank@gmail.com>
2019-12-31 11:25:20 -0500

    cleanup

--- boostTRF.py
+++ boostTRF.py
@@ -19,115 +19,33 @@ configure(n_workers=8)
 
 time1 = time.time()
 
-outfolder = 'HFVolFIR_30_130'
-
-NAAflag = False
-
-if NAAflag:
-    outfolder = outfolder + 'NAA'
-
-if socket.gethostname() == 'ISRWKAVW2269C.local':
-    outfolder += 'AVW'
-    if NAAflag:
-        sqdFolder = '/Users/pranjeevan/Documents/HAYO/NAAraw/QuietsDen'
-    else:
-        sqdFolder = '/Users/pranjeevan/Documents/HAYO/DenoiseQuiet'
-    stimuliFolder = '/Users/pranjeevan/Documents/HAYO/stimuliQuiet'
-    outputFolder = '/Users/pranjeevan/Documents/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/Users/pranjeevan/Documents/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/Users/pranjeevan/Documents/HAYO/sourcespace/empty room data'
-    mriFolder = '/Users/pranjeevan/Documents/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = False
-elif socket.gethostname() == 'asterix.isr.umd.edu':
-    outfolder += 'AST'
-    if NAAflag:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/NAAQuiet'
-    else:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/DenoiseQuiet'
-    stimuliFolder = '/export/joshua/HAYO/stimuli/stimuliQuiet'
-    outputFolder = '/export/joshua/results/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/export/joshua/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/export/joshua/SCHZ/emptyroomdata'
-    mriFolder = '/export/joshua/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = True
-elif socket.gethostname() == 'obelix.isr.umd.edu':
-    outfolder += 'OBE'
-    if NAAflag:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/NAAQuiet'
-    else:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/DenoiseQuiet'
-    #sqdFile = '/Users/pranjeevan/Documents/Schizo/MEGDATA/DenoiseData/R2262_CocktailSZmyF.sqd'
-    stimuliFolder = '/export/joshua/HAYO/stimuli/stimuliQuiet'
-    outputFolder = '/export/joshua/results/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/export/joshua/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/export/joshua/HAYO/emptyroomdata'
-    mriFolder = '/export/joshua/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = True
-elif socket.gethostname() == 'cacofonix.isr.umd.edu':
-    outfolder += 'CAC'
-    if NAAflag:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/NAAQuiet'
-    else:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/DenoiseQuiet'
-    #sqdFile = '/Users/pranjeevan/Documents/Schizo/MEGDATA/DenoiseData/R2262_CocktailSZmyF.sqd'
-    stimuliFolder = '/export/joshua/HAYO/stimuli/stimuliQuiet'
-    outputFolder = '/export/joshua/results/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/export/joshua/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/export/joshua/HAYO/emptyroomdata'
-    mriFolder = '/export/joshua/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = True
-elif socket.gethostname() == 'panacea.isr.umd.edu':
-    outfolder += 'PAN'
-    if NAAflag:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/NAAQuiet'
-    else:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/DenoiseQuiet'
-    #sqdFile = '/Users/pranjeevan/Documents/Schizo/MEGDATA/DenoiseData/R2262_CocktailSZmyF.sqd'
-    stimuliFolder = '/export/joshua/HAYO/stimuli/stimuliQuiet'
-    outputFolder = '/export/joshua/results/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/export/joshua/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/export/joshua/HAYO/emptyroomdata'
-    mriFolder = '/export/joshua/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = True
-elif socket.gethostname() == 'geriatrix.isr.umd.edu':
-    outfolder += 'GER'
-    if NAAflag:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/NAAQuiet'
-    else:
-        sqdFolder = '/export/joshua/HAYO/MEGdata/DenoiseQuiet'
-    #sqdFile = '/Users/pranjeevan/Documents/Schizo/MEGDATA/DenoiseData/R2262_CocktailSZmyF.sqd'
-    stimuliFolder = '/export/joshua/HAYO/stimuli/stimuliQuiet'
-    outputFolder = '/export/joshua/results/HighFreq/ResultsNew2/' + outfolder
-    forwardSolFolder = '/export/joshua/HAYO/sourcespace/fwdpkl'
-    emptyroomFolder = '/export/joshua/HAYO/emptyroomdata'
-    mriFolder = '/export/joshua/HAYO/sourcespace/mri'
-    aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
-    astflag = True
-
-else:
-    raise OSError('INVALID HOSTNAME')
+outfolder = 'results'
+sqdFolder = '/Users/pranjeevan/Documents/highfreqTRF/sqd_folder'
+stimuliFolder = '/Users/pranjeevan/Documents/highfreqTRF/stimuli_folder'
+outputFolder = '/Users/pranjeevan/Documents/highfreqTRF/output_folder' + outfolder
+forwardSolFolder = '/Users/pranjeevan/Documents/highfreqTRF/sourcespace/fwdpkl'
+emptyroomFolder = '/Users/pranjeevan/Documents/highfreqTRF/sourcespace/empty room data'
+mriFolder = '/Users/pranjeevan/Documents/highfreqTRF/sourcespace/mri'
+aparcLabels = ['superiortemporal-lh','superiortemporal-rh','transversetemporal-lh','transversetemporal-rh']
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
+temp = f'{outputFolder}/Source/Pickle'
+if not os.path.exists(temp):
+    os.makedirs(temp)
+if os.path.exists(f'{outputFolder}/Source/boosting.txt'):
+    os.remove(f'{outputFolder}/Source/boosting.txt')
 
 srcspace_name = 'vol-7-cortex_brainstem_full'
 parc_name = f'parc_sym-aparc+aseg-nowhite-{srcspace_name}.pkl'
-src1_name = 'vol-7'
-src2_name = 'vol-7-brainstem'
 
 nowhite = True
 ctxll = ['inferiortemporal', 'middletemporal',
                        'superiortemporal','bankssts', 'transversetemporal']
-
 subctxll = ['Brain-Stem', '3rd-Ventricle','Thalamus-Proper', 'VentralDC']
-
 llA = ['ctx-lh-'+l for l in ctxll] + ['ctx-rh-'+l for l in ctxll] + subctxll + ['Left-'+l for l in subctxll] + ['Right-'+l for l in subctxll]
-
+aparcvols = load.unpickle('aparcvol.pkl')
 
 decimation = 1   #when loading data
-dssfilter = [70,300]
 datafilter = [70,300]
 nperm = 3
 ltranswidth = 10
@@ -138,45 +56,30 @@ basislen = 0.004
 datatime = [0,60]
 
 print('loading stimuli')
-
 matstimfolder = stimuliFolder
-envelopes = {}
 carrier = {}
 yangh = {}
 
 mat7 = scipy.io.loadmat(f'{matstimfolder}/quiet_h-audspec-1000.mat')
 mat8 = scipy.io.loadmat(f'{matstimfolder}/quiet_l-audspec-1000.mat')
-
 freqs = Scalar('frequency',mat7['frequencies'][0],'Hz')
-
 spec7 = NDVar(mat7['specgram'],dims=(UTS(0,0.001,60000),freqs))
 spec8 = NDVar(mat8['specgram'],dims=(UTS(0,0.001,60000),freqs))
-
 spec7 = spec7.sub(frequency=(300,4000))
 spec8 = spec8.sub(frequency=(300,4000))
-
 spec7filt= filter_data(spec7,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
 spec8filt= filter_data(spec8,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
-
 filt7 = spec7filt.mean('frequency')
 filt8 = spec8filt.mean('frequency')
-
 yangh[7]= filter_data(filt7,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
 yangh[8]= filter_data(filt8,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
 
 wav7 = load.wav('%s/quiet_h.wav' % stimuliFolder)
 wav7.x = wav7.x.astype('float64')
-env7 = wav7.envelope()
-env7 = resample(env7,1000)
 wav7 = resample(wav7,1000)
-
 wav8 = load.wav('%s/quiet_l.wav' % stimuliFolder)
 wav8.x = wav8.x.astype('float64')
-env8 = wav8.envelope()
-env8 = resample(env8,1000)
 wav8 = resample(wav8,1000)
-
-
 carrier[7] = filter_data(wav7,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
 carrier[8] = filter_data(wav8,datafilter[0],datafilter[1],filter_length='auto',method='fir',fir_design='firwin',l_trans_bandwidth = ltranswidth,h_trans_bandwidth = htranswidth)
 
@@ -184,52 +87,18 @@ mat = scipy.io.loadmat('matlabOutHAYO.mat')
 badchannelsAll = mat['badChannels'][0]
 badchannelsNames = mat['subjectsAll'][0]
 
-aparcvols = load.unpickle('aparcvol.pkl')
-
 subjectFolders = [f for f in listdir(sqdFolder) if ~isfile(join(sqdFolder,f))]
 for f in reversed(subjectFolders):
     if(f[0]=='.'):              #remove hidden files like .DS_Store
         subjectFolders.remove(f)
-
 subjectFolders.sort()
-
-if not os.path.exists(outputFolder):
-    os.makedirs(outputFolder)
-temp = '%s/Source/Pickle' % outputFolder
-if not os.path.exists(temp):
-    os.makedirs(temp)
 
 rawE1 = mne.io.read_raw_kit(join(emptyroomFolder,'EmptyRoom_PreAM-2_08.02.16.sqd'),preload=True)
 rawE2 = mne.io.read_raw_kit(join(emptyroomFolder,'ERT_07.25.14_4.14.sqd'),preload=True)
 
-if os.path.exists(f'{outputFolder}/Source/boosting.txt'):
-    os.remove(f'{outputFolder}/Source/boosting.txt')
-
 fnum = 1
 fflag = 1
 for subjectF in subjectFolders:
-    # #
-    if(subjectF=='R2336'):
-        fnum +=1
-        continue
-    
-    if(subjectF=='R2411'):
-        fnum +=1
-        continue
-    # if(subjectF!='R2083'):
-    #     fnum +=1
-    #     continue
-
-    # if fnum<=:
-    #     fnum +=1
-    #     continue
-
-    # if(fnum>5):
-    #     break
-    # if subjectF != 'R2083':
-    #     fnum +=1
-    #     continue
-
     t = time.time()
     subjectFolder = '%s/%s' %(sqdFolder,subjectF)
     print('\n\nsubject %d of %d : %s' % (fnum,len(subjectFolders),subjectFolder))
@@ -242,18 +111,11 @@ for subjectF in subjectFolders:
     print('loading file : %s' %(sqdFileL))
     rawL = mne.io.read_raw_kit(sqdFileL,stim_code='channel', stim=range(163,176),preload=True)
 
-    if NAAflag:
-        mat = scipy.io.loadmat(join(subjectFolder, f'{subjectF}_QHbadC.mat'))
-        badCH= mat['badChannels'][0]
-        mat = scipy.io.loadmat(join(subjectFolder, f'{subjectF}_QLbadC.mat'))
-        badCL= mat['badChannels'][0]
-
-    else:
-        for j in range(0,len(badchannelsNames)):
-            if (badchannelsNames[j][0] == subjectF):
-                break
-        badCH = badchannelsAll[j+8][0]
-        badCL = badchannelsAll[j+9][0]
+    for j in range(0,len(badchannelsNames)):
+        if (badchannelsNames[j][0] == subjectF):
+            break
+    badCH = badchannelsAll[j+8][0]
+    badCL = badchannelsAll[j+9][0]
 
     if(len(badCH)!=0):
         badCH = [e + 1 for e in badCH]
@@ -266,7 +128,6 @@ for subjectF in subjectFolders:
         for i in range(0, len(badCL)):
             rawL.info['bads'].append('MEG %03d' % (badCL[i]))
         print(rawL.info['bads'])
-
 
     if(rawH.info['kit_system_id']==rawE1.info['kit_system_id']):
         rawE = mne.io.RawArray(rawE1.get_data(),rawE1.info)
